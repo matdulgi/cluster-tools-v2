54f83979e671f1e4f7ffe99f72bb17d65e16b741
yura <yrdog9494@gmail.com>
2024-01-06 20:13:15 +0900

    Copy, Sync

---
+++ src/main/scala/dulgi/clustertools/task/Copy.scala
@@ -0,0 +1,48 @@
+package dulgi.clustertools.task
+
+import dulgi.clustertools.env.Node
+import scala.sys.process.ProcessLogger
+import scala.sys.process._
+
+/**
+ * Cluster Copy
+ *
+ * Copy File from Source Node to Destination Nodes
+ * File will not be copied when Source node is Destination node
+ *
+ * @param targetNode
+ * @param args
+ */
+class Copy(targetNode: Node, args: Seq[String]) extends Task(targetNode){
+
+  /**
+   * check file already exists in target node
+   * it will make prompt when file exists in at least once node
+   * @return
+   */
+  def seek(): Boolean = {
+    true
+  }
+
+  override def execute(): TaskResult = {
+    val (sourcePath, destPath) = args match {
+      case Seq() => throw new IllegalArgumentException("no args")
+      case Seq(arg1: String) => (arg1, arg1)
+      case Seq(arg1: String, arg2: String) => (arg1, arg2)
+      case Seq(arg1: String, arg2: String, _*) => throw new IllegalArgumentException(s"more then two args: $args")
+    }
+
+    val sshCommand = Seq("scp", "-P", targetNode.port, sourcePath, s"${targetNode.user}@${targetNode.hostname}:$destPath")
+    val (outputBuffer, errorBuffer) = (new StringBuilder, new StringBuilder)
+    val logger = ProcessLogger(
+      (o: String) => outputBuffer.append(o + "\n"),
+      (e: String) => errorBuffer.append(e + "\n")
+    )
+
+    logger.out(s"task started in ${targetNode.name} for command: [ ${sshCommand.mkString(" ")} ]")
+    logger.err(s"task started in ${targetNode.name} for command: [ ${sshCommand.mkString(" ")} ]")
+    val exitCode = sshCommand ! logger
+
+    SequentialTaskResult(targetNode.name, exitCode, outputBuffer.toString, errorBuffer.toString)
+  }
+}
---
+++ src/main/scala/dulgi/clustertools/task/Sync.scala
@@ -0,0 +1,40 @@
+package dulgi.clustertools.task
+
+import dulgi.clustertools.env.Node
+import scala.sys.process.ProcessLogger
+import scala.sys.process._
+
+
+class Sync(override val targetNode: Node, args: Seq[String]) extends Task(targetNode){
+  override def execute(): TaskResult = {
+    val (sourcePath, destPath) = args match {
+      case Seq() => throw new IllegalArgumentException("no args")
+      case Seq(arg1: String) => (arg1, arg1)
+      case Seq(arg1: String, arg2: String) => (arg1, arg2)
+      case Seq(arg1: String, arg2: String, _*) => throw new IllegalArgumentException(s"more then two args: $args")
+    }
+
+    val sshCommand = Seq("rsync", "-avh", "-e", s"'ssh -p ${targetNode.port}'", targetNode.port, s"${targetNode.user}@${targetNode.hostname}:$sourcePath", destPath)
+
+    val outputBuffer = new StringBuilder
+    val errorBuffer = new StringBuilder
+
+    val logger = ProcessLogger(
+      (o: String) => outputBuffer.append(o + "\n"),
+      (e: String) => errorBuffer.append(e + "\n")
+    )
+
+    val process = sshCommand.run()
+    val exitCode = process.exitValue()
+
+    val stdout = outputBuffer.toString()
+    val stderr = errorBuffer.toString()
+
+    SequentialTaskResult(targetNode.name, exitCode, stdout, stderr)
+  }
+
+  override def onStart(): Unit = println()
+  override def onFinish(): Unit = println()
+}
+
+
